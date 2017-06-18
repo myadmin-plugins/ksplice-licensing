4bc386c48d93c59393988e51cd8a296ed43036ed
Joe Huss <detain@interserver.net>
2017-06-18 01:47:33 -0400

    fixs to camelCase for psr

--- src/Plugin.php
+++ src/Plugin.php
@@ -19,7 +19,7 @@ class Plugin {
 	public static function getHooks() {
 		return [
 			'function.requirements' => [__CLASS__, 'Requirements'],
-			'licenses.settings' => [__CLASS__, 'Settings'],
+			'licenses.settings' => [__CLASS__, 'getSettings'],
 			'licenses.activate' => [__CLASS__, 'Activate'],
 			'licenses.deactivate' => [__CLASS__, 'Deactivate'],
 		];
@@ -91,7 +91,7 @@ class Plugin {
 		$loader->add_requirement('deactivate_ksplice', '/../vendor/detain/myadmin-ksplice-licensing/src/ksplice.inc.php');
 	}
 
-	public static function Settings(GenericEvent $event) {
+	public static function getSettings(GenericEvent $event) {
 		// will be executed when the licenses.settings event is dispatched
 		$settings = $event->getSubject();
 		$settings->add_text_setting('licenses', 'KSplice', 'ksplice_api_username', 'Ksplice API Username:', 'Ksplice API Username', $settings->get_setting('KSPLICE_API_USERNAME'));
